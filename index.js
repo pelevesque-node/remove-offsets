911cc681cc7c7d97783c61bae5ed6a43bbc698fc
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2019-05-30 22:41:50 -0400

    betterfied for loops

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const isOffset = require('@pelevesque/is-offset')
 
 module.exports = (arr, step = 1) => {
   let indicesToRemove = []
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0, len = arr.length - 1; i < len; i++) {
     if (indicesToRemove.indexOf(i) === -1) {
-      for (let j = i + 1; j < arr.length; j++) {
+      for (let j = i + 1, len = arr.length; j < len; j++) {
         if (isOffset(arr[i], arr[j], step)) {
           indicesToRemove.push(j)
         }
